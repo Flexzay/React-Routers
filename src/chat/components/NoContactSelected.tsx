dddf93fe09d6a718000ff9ed96bb6a96007f4b8a
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-12-17 01:32:20 -0500

    separacion de componentes adicionales

---
+++ src/chat/components/NoContactSelected.tsx
@@ -0,0 +1,10 @@
+export const NoContactSelected = () => {
+  return (
+     <div className="flex h-full w-full items-center justify-center">
+        <div className="text-center">
+            <h1 className=" text-2xl font-bold"> No Contact select</h1>
+            <p className="text-muted-foreground"> select a chat from the siebar</p>
+        </div>
+     </div>
+  )
+}
