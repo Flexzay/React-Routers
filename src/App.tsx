e13d19c67d2cff35243a041d226486f0dea38957
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-12-18 00:45:25 -0500

    mostrar listado de clientes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { AppRouter } from "./AppRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <AppRouter />
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 };
